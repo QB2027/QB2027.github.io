e9adfec84fa0e7b6ecd48ce8fc5ba4fbdf1fa47c
jasonxue1 <jason@xuechunxi.com>
2025-01-04 22:12:42 +0800

    re

--- static/js/fileManager.js
+++ static/js/fileManager.js
@@ -10,27 +10,27 @@ export function loadFiles(jsonPath) {
 
 function renderFileTree(files, parentElement, parentRepo = "", parentBranch = "main") {
     files.forEach(file => {
-        const li = document.createElement('li');
-
-        if (file.children) {
-            // 如果有子文件，递归渲染子文件
+        if (file.children && file.children.length > 0) {
+            // 递归处理非空的子文件夹
             renderFileTree(file.children, parentElement, file.repo || parentRepo, file.branch || parentBranch);
-        } else if (file.path) {
-            // 仓库文件：生成原始下载和镜像下载链接
-            const repo = parentRepo;
-            const branch = file.branch || parentBranch;
-            const rawLink = `https://raw.githubusercontent.com/${repo}/${branch}/${file.path}`;
-            const mirrorLink = `https://ghproxy.com/https://raw.githubusercontent.com/${repo}/${branch}/${file.path}`;
-            li.innerHTML = `
-                <span>${file.name}</span>
-                <a href="${rawLink}" target="_blank" style="margin-left: 10px;">原始下载</a>
-                <a href="${mirrorLink}" target="_blank" style="margin-left: 10px; color: green;">镜像下载</a>
-            `;
-        } else if (file.url) {
-            // 外部链接文件
-            li.innerHTML = `<a href="${file.url}" target="_blank">${file.name} (外部链接)</a>`;
+        } else if (file.path || file.url) {
+            const li = document.createElement('li');
+            if (file.path) {
+                // 仓库文件：生成原始下载和镜像下载链接
+                const repo = parentRepo;
+                const branch = file.branch || parentBranch;
+                const rawLink = `https://raw.githubusercontent.com/${repo}/${branch}/${file.path}`;
+                const mirrorLink = `https://ghproxy.com/https://raw.githubusercontent.com/${repo}/${branch}/${file.path}`;
+                li.innerHTML = `
+                    <span>${file.name}</span>
+                    <a href="${rawLink}" target="_blank" style="margin-left: 10px;">原始下载</a>
+                    <a href="${mirrorLink}" target="_blank" style="margin-left: 10px; color: green;">镜像下载</a>
+                `;
+            } else if (file.url) {
+                // 外部链接文件
+                li.innerHTML = `<a href="${file.url}" target="_blank">${file.name} (外部链接)</a>`;
+            }
+            parentElement.appendChild(li);
         }
-
-        parentElement.appendChild(li);
     });
 }
